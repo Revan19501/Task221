d7e22b52ca3a9bf50da2d6478ca539f887b4bb46
Dinar Z <4.ak@bk.ru>
2025-02-17 17:21:29 +0300

    Initial commit

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,46 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Transactional
+    @Override
+    public void delete(User user) {
+        sessionFactory.getCurrentSession().delete(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public User getUser(String model, int series) {
+        String hql = "SELECT u FROM User u JOIN u.car c WHERE c.model = :model AND c.series = :series";
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
+        query.setParameter("model", model);
+        query.setParameter("series", series);
+
+        return (User) query.getSingleResult();
+    }
+
+}
